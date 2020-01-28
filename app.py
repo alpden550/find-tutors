99df3ec4e04c1d2f6f7eaf46d677217e5a6d3659
alpden550 <alpden550@gmail.com>
2020-01-28 09:52:51 +0300

    Added tutor's info page.

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
-from flask import Flask
+import json
+from pathlib import Path
+
+from flask import Flask, render_template
 
 app = Flask(__name__)
+
+TUTORS_JSON = 'tutors.json'
 
 
 @app.route('/')
@@ -13,9 +18,15 @@ def goals(goal):
     return goal
 
 
-@app.route('/profiles/<int:teacher_id>/')
-def teachers(teacher_id):
-    return 'Teacher %d' % teacher_id
+@app.route('/profiles/<int:tutor_id>/')
+def tutors(tutor_id):
+    tutors_data = json.loads(Path(TUTORS_JSON).read_text())
+    all_goals = tutors_data.get('goals')
+    all_tutors = tutors_data.get('teachers')
+    tutor = next(tutor for tutor in all_tutors if tutor['id'] == tutor_id)
+    tutor_goals = [all_goals[goal] for goal in tutor['goals']]
+
+    return render_template('profile.html', tutor=tutor, goals=tutor_goals)
 
 
 @app.route('/request/')
